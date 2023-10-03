1ffac4c09569d2760ee0e0d27a5adad46f2e4777
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-10-03 10:52:01 +0545

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -37,4 +37,4 @@ class Var(object):
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://HemantC1:HemantC1@cluster0.hdjjse6.mongodb.net/?retryWrites=true&w=majority"))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Sujan_BotZ'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001722524017")).split())) 
